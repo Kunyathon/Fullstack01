90a502ec5a2554e03f66064fa7be2936d4ca8d49
Kunyathon <tykunya@gmail.com>
2025-03-26 11:02:45 +0700

    update login page ty

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -21,8 +21,16 @@ const onSubmit = async (values) => {
 </script>
 
 <template>
-    <main class="m-auto w-[300px]">
-        <LoginForm @submit="onSubmit" />
-        <BaseErrorMessage>{{ store.userError }}</BaseErrorMessage>         
+    <main class="relative w-full h-screen flex items-center justify-center">
+        <video class="absolute top-0 left-0 w-full h-full object-cover" autoplay loop muted>
+            <source src="assets/videoes/lofi-anime.mp4" type="video/mp4" />
+        </video>
+        <div class="absolute flex justify-center items-center s w-[500px] h-[300px] bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+             <div>
+                 <LoginForm @submit="onSubmit" />
+        <BaseErrorMessage>{{ store.userError }}</BaseErrorMessage>
+             </div>  
+         
+        </div>
     </main>
 </template>
